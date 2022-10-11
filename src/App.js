1b8406e9437e6b77688dd4d1c464dc39ed37e52d
dinasoa <hei.dinasoa@gmail.com>
2022-10-11 11:57:12 +0300

    feat: adding header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-
+import { Header } from "./components/Header";
+import './styles/header.css' ;
 function App() {
   return (
     <div className="App">
-      <h1>Hello World !</h1>
+      <Header/>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+export const Header = () => {
+    return (
+        <>
+            <header class="header">
+                <a href="" class="logo">Library</a>
+                <ul class="menu">
+                    <li><a href="#booklist">Book list</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </header>
+        </>
+    )
+}
